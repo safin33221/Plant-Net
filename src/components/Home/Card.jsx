3065eab10433ee8473c5c6dfccd548e5a8a612de
safin33221 <saifn33221@gmail.com>
2025-02-02 19:53:13 +0600

    completed to plants in card formate

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const Card = () => {
+const Card = ({ plant }) => {
   return (
     <Link
-      to={`/plant/1`}
+      to={`/plant/${plant.id}`}
       className='col-span-1 cursor-pointer group shadow-xl p-3 rounded-xl'
     >
       <div className='flex flex-col gap-2 w-full'>
@@ -24,8 +25,8 @@ const Card = () => {
                 group-hover:scale-110 
                 transition
               '
-            src='https://i.ibb.co.com/rMHmQP2/money-plant-in-feng-shui-brings-luck.jpg'
-            alt='Plant Image'
+            src={plant.image}
+            alt={plant.name}
           />
           <div
             className='
@@ -35,15 +36,27 @@ const Card = () => {
             '
           ></div>
         </div>
-        <div className='font-semibold text-lg'>Money Plant</div>
-        <div className='font-semibold text-lg'>Category: Indoor</div>
-        <div className='font-semibold text-lg'>Quantity: 10</div>
+        <div className='font-semibold text-lg'>{plant.name}</div>
+        <div className='font-semibold text-lg'>Category: {plant.category}</div>
+        <div className='font-semibold text-lg'>Quantity: {plant.quantity}</div>
         <div className='flex flex-row items-center gap-1'>
-          <div className='font-semibold'> Price: 15$</div>
+          <div className='font-semibold'> Price: {plant.price}$</div>
         </div>
       </div>
     </Link>
   )
 }
 
+Card.propTypes = {
+  plant: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+}
+
 export default Card
+
